1d59c4f4f951f4aff7d914beecd5e2d112821552
Dennis Hagenäs <dh_lexicon@outlook.com>
2022-12-12 15:03:00 +0100

    finished the bish bosh part of exercise 9

--- ovning9/bishbosh.js
+++ ovning9/bishbosh.js
@@ -1,33 +1,38 @@
+let result = document.querySelector('#result');
+let iField = document.querySelector('#i');
+let jField = document.querySelector('#j');
+let nField = document.querySelector('#n');
+let form = document.querySelector('#inputForm');
+let submit_btn = document.querySelector('#submit_btn');
+
+
+iField.value = 3;
+jField.value = 4;
+nField.value = 100;
+result.innerHTML = '  ';
 
 
 
-console.log('hej');
+const submit_action = () => {
+    let i = iField.value;
+    let j = jField.value;
+    let n = nField.value;
+    result.innerHTML = '  ';
 
-let name = 'Nisse';
-
-console.log(name);
-
-function hej(){
-    console.log('hej');
+    for (let index = 1; index <= n; index++) {
+        if (((index % i)==0) && ((index % j)==0)) result.innerHTML += '<br>bish bosh</br>';
+        else
+        if ((index % i)==0)  result.innerHTML += '<br>bish</br>';
+        else
+        if ((index % j)==0) result.innerHTML += '<br>bosh</br>';
+        else result.innerHTML += `<br>${index}</br>`;
+    }
 }
 
-function hej2(){
-    console.log('hej2');
-}
+submit_btn.addEventListener('click', function(e){
+    e.preventDefault();
+    submit_action()
+});
 
-function calc(num1, num2){
-    const sum =  num1 + num2;
-    console.log(sum);
-    return sum;
-}
 
-hej();
 
-hej2();
-
-let sum = calc(3, 5);
-console.log(sum);
-
-const calc3 = (num1 = 0, num2 = 0) => num1 + num2;
-
-console.log(calc3(3, 4));
